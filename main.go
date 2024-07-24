00ebc3ce2394bbd2ed366416ffb7b8747576e986
sysadmin-umar <m.umerpervaiz@gmail.com>
2024-07-24 20:24:09 +0500

    project added

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello, %s!", name)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("ListenAndServe: %v", err)
+	}
+}
